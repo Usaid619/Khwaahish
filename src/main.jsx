68d59b5f61ea36e45c24ac44ff8b369890b6eb9c
Usaid <mohdu3106@gmail.com>
2025-01-22 00:55:22 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,58 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Product from './Component-Diamond/Product.jsx'
+import Jhumka from './Component-Diamond/Pages/Jhumka.jsx'
+import PrincessPossessionDiamondEarrings from './Component-Diamond/Pages/PrincessPossessionDiamondEarrings';
+import HeroMain from './Hero Component/HeroMain';
+import { Provider } from 'react-redux'
+import { store } from './App/store.js'
+import OurStory from './OurStory.jsx'
+import NoorPage from './components conoor/NoorPage.jsx';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <HeroMain />,
+      },
+      {
+        path: 'product',
+        element: <Product />,
+      },
+      {
+        path: 'jhumka',
+        element: <Jhumka />,
+      },
+      {
+        path: 'princess-possession-diamond-earrings',
+        element: <PrincessPossessionDiamondEarrings />,
+      },{
+        path:'story',
+        element:<OurStory/>
+      },
+      {
+        path:'noor',
+        element:<NoorPage/>
+      }
+    ]
+  }
+],
+{
+  basename: '/khwaahish/', // Replace '/subdir' with your desired base path
+})
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
+  </StrictMode>,
+)
